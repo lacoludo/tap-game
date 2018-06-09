ee7d4842b680d997a2d6b77085b43c5a941ad4ab
LACOUTURE Ludovic <email@lacoludo.com>
2018-06-09 06:15:39 +0200

    server updated 2

--- server/app.js
+++ server/app.js
@@ -5,34 +5,33 @@
 
  */
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+import express from "express";
+import path from "path";
+import favicon from "serve-favicon";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
 // [SH] Require Passport
-var passport = require('passport');
+import passport from "passport";
 
 // [SH] Bring in the data model
-require('./api/models/db');
+require("./api/models/db");
 // [SH] Bring in the Passport config after model is defined
-require('./api/config/passport');
-
+require("./api/config/passport");
 
 // [SH] Bring in the routes for the API (delete the default routes)
-var routesApi = require('./api/routes/index');
+const routesApi = require("./api/routes/index");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,46 +41,45 @@ app.use(cors());
 app.use(passport.initialize());
 
 // [SH] Use the API routes when path starts with /api
-app.use('/api', routesApi);
+app.use("/api", routesApi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 
 // [SH] Catch unauthorised errors
-app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
     res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
+    res.json({ message: err.name + ": " + err.message });
   }
 });
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
 });
 
-
-module.exports = app;
+export default app;
